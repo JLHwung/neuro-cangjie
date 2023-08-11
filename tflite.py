1aeb718623aed07c67bc0edbc7081791ecb457eb
Huáng Jùnliàng <jlhwung@gmail.com>
2023-08-11 15:54:11 -0400

    bump tensorflow-model-optimization

--- tflite.py
+++ tflite.py
@@ -18,5 +18,5 @@ converter.target_spec.supported_types = [tf.float16]
 tflite_model = converter.convert()
 
 # Save the model.
-with open('cangjie.tflite', 'wb') as f:
+with open('cangjie_pruned.tflite', 'wb') as f:
     f.write(tflite_model)
